b57972f0e191b3e2976b8129caf3c017306268fd
zephyrus21 <pandey.piyus@gmail.com>
2020-09-08 14:10:26 +0530

    clean up

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCVPBK2Rl2K3Quh2ce8L_y6YpJKVVJ0WBI',
+    authDomain: 'howsapp-p21.firebaseapp.com',
+    databaseURL: 'https://howsapp-p21.firebaseio.com',
+    projectId: 'howsapp-p21',
+    storageBucket: 'howsapp-p21.appspot.com',
+    messagingSenderId: '538905099910',
+    appId: '1:538905099910:web:b913ed8679597f52405b07',
+    measurementId: 'G-HN75Y40Q3P',
+};
